b80bab53ab1b9ecc5147ac1bbc26760aa2005bbe
Kiz-uesp <carrickk95@gmail.com>
2011-12-06 21:31:40 +0000

    !blames

--- peregrine.py
+++ peregrine.py
@@ -611,6 +611,12 @@ def onPubmsg(connection, event):
                 connection.action(channel, 'hangs head in shame.')
             else:
                 connection.action(channel, 'blames ' + blamed)
+        if lowm.startswith('!blames ') and len(lwords)>1:
+            blamed=' '.join(words[1:])
+            if blamed.lower() == connection.get_nickname().lower():
+                connection.action(channel, 'hangs head in shame.')
+            else:
+                connection.action(channel, 'blames ' + blamed)
 ##        if lowm.startswith('!oldest ') and len(lwords)==2:
 ##            if lwords[1].isdigit():
 ##                num=int(lwords[1])
